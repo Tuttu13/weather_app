a7a3b76c6e099574d4a7759f780d5a3e671fb1fc
Tuttu13 <tutui0426@yahoo.co.jp>
2024-05-30 22:30:41 +0900

    天気表示画面　追加

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Paper from "@mui/material/Paper";
+import Select from "@mui/material/Select";
+import Table from "@mui/material/Table";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>天気を表示する小さいアプリの実装</h1>
+        <div>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="demo-simple-select-label">都市選択</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Prefecture">
+              <MenuItem value={"SAPPORO"}>札幌</MenuItem>
+              <MenuItem value={"SENDAI"}>仙台</MenuItem>
+              <MenuItem value={"TOKYO"}>東京</MenuItem>
+              <MenuItem value={"YOKOHAMA"}>横浜</MenuItem>
+              <MenuItem value={"OSAKA"}>大阪</MenuItem>
+              <MenuItem value={"KYOTO"}>京都</MenuItem>
+              <MenuItem value={"KOBE"}>神戸</MenuItem>
+              <MenuItem value={"NAGOYA"}>名古屋</MenuItem>
+              <MenuItem value={"HIROSHIMA"}>広島</MenuItem>
+              <MenuItem value={"HUKUOKA"}>福岡</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div>
+          <h2>天気情報</h2>
+          <Paper sx={{
+            width: 1500,
+          }}>
+            <Table
+              stickyHeader
+              style={{
+                tableLayout: "fixed",
+              }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>都市名</TableCell>
+                  <TableCell>気温（摂氏）</TableCell>
+                  <TableCell>天気</TableCell>
+                  <TableCell>風速</TableCell>
+                  <TableCell>風向き</TableCell>
+                  <TableCell>湿度</TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </Paper>
+        </div>
     </div>
   );
 }
-
 export default App;
