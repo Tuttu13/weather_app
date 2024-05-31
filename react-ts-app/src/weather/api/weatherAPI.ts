4007bf5afec7c9ae57d40b0d3d19378a0896c6c5
Tuttu13 <tutui0426@yahoo.co.jp>
2024-05-31 23:00:13 +0900

    apiファイル　追加

---
+++ react-ts-app/src/weather/api/weatherAPI.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useEffect } from "react";
+import { WeatherData } from "../types";
+
+export const useFetchWeatherData = (
+  city: string,
+  setWeatherData: React.Dispatch<React.SetStateAction<WeatherData | null>>
+) => {
+  useEffect(() => {
+    if (city) {
+      axios
+        .get<WeatherData>(
+          `${process.env.REACT_APP_OW_API_URL}?q=${city}&lang=${process.env.REACT_APP_OW_API_LANG}&appid=${process.env.REACT_APP_OW_API_KEY}`
+        )
+        .then((response: AxiosResponse<WeatherData>) => {
+          setWeatherData(response.data);
+        })
+        .catch((error: AxiosError) => {
+          console.error("Error fetching the weather data", error);
+        });
+    }
+  }, [city]);
+};
