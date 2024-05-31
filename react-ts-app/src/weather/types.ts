9139f2ab0dd4af49ee6165a06c189bb70aaf1c7b
Tuttu13 <tutui0426@yahoo.co.jp>
2024-05-31 23:00:40 +0900

    定数ファイル　追加

--- react-ts-app/src/weather/types.ts
+++ react-ts-app/src/weather/types.ts
@@ -1,16 +1,3 @@
-export const cities = [
-  { value: "SAPPORO", label: "札幌" },
-  { value: "SENDAI", label: "仙台" },
-  { value: "TOKYO", label: "東京" },
-  { value: "YOKOHAMA", label: "横浜" },
-  { value: "OSAKA", label: "大阪" },
-  { value: "KYOTO", label: "京都" },
-  { value: "KOBE", label: "神戸" },
-  { value: "NAGOYA", label: "名古屋" },
-  { value: "HIROSHIMA", label: "広島" },
-  { value: "FUKUOKA", label: "福岡" },
-];
-
 export interface WeatherData {
   name: string;
   main: {
@@ -25,3 +12,8 @@ export interface WeatherData {
     deg: number;
   };
 }
+export interface City {
+  value: string;
+  label: string;
+}
+[];
