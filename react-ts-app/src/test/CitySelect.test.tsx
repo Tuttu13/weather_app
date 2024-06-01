64d9868051283168de77e2643e821ed353a47f54
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-06-01 13:44:15 +0900

    コンポーネントテスト　追加

---
+++ react-ts-app/src/test/CitySelect.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import CitySelect from '../weather/components/CitySelect';
+
+// モックの都市データ
+const cities = [
+  { value: 'SAPPORO', label: '札幌' },
+  { value: 'SENDAI', label: '仙台' },
+  { value: 'TOKYO', label: '東京' },
+  { value: 'YOKOHAMA', label: '横浜' },
+  { value: 'OSAKA', label: '大阪' },
+  { value: 'KYOTO', label: '京都' },
+  { value: 'KOBE', label: '神戸' },
+  { value: 'NAGOYA', label: '名古屋' },
+  { value: 'HIROSHIMA', label: '広島' },
+  { value: 'FUKUOKA', label: '福岡' },
+];
+
+test('CitySelect component renders correctly', () => {
+  const selectedCity = 'TOKYO';
+  const mockOnChange = jest.fn();
+
+  render(
+    <CitySelect
+      cities={cities}
+      selectedCity={selectedCity}
+      onCityChange={mockOnChange}
+    />
+  );
+
+  // セレクトボックスのラベルを取得
+  const selectLabel = screen.getByLabelText('都市選択');
+
+  // セレクトボックスが正しく表示されていることを確認
+  expect(selectLabel).toBeInTheDocument();
+});
---
+++ react-ts-app/src/test/WeatherTable.test.tsx
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import WeatherTable from '../weather/components//WeatherTable';
+
+describe('WeatherTable component', () => {
+  const weatherData = {
+    name: '東京都',
+    main: {
+      temp: 300,
+      humidity: 80,
+    },
+    weather: [{ description: '曇り' }],
+    wind: { speed: 5, deg: 180 },
+  };
+
+  it('renders weather data correctly', () => {
+    render(<WeatherTable weatherData={weatherData} />);
+
+    // 都市名が表示されていることを確認
+    expect(screen.getByText('東京都')).toBeInTheDocument();
+
+    // 気温が摂氏で表示されていることを確認
+    expect(screen.getByText('26.9 °C')).toBeInTheDocument();
+
+    // 天気が表示されていることを確認
+    expect(screen.getByText('曇り')).toBeInTheDocument();
+
+    // 風速が表示されていることを確認
+    expect(screen.getByText('5 m/s')).toBeInTheDocument();
+
+    // 風向きが表示されていることを確認
+    expect(screen.getByText('180 °')).toBeInTheDocument();
+
+    // 湿度が表示されていることを確認
+    expect(screen.getByText('80 %')).toBeInTheDocument();
+  });
+});
