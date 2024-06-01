7929aa0a2ac4dccd2cf9cc7e8cbfdbf0b5f58ba2
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-06-01 10:53:55 +0900

    天気データ取得時のログ出力　追加

--- react-ts-app/src/weather/api/weatherAPI.ts
+++ react-ts-app/src/weather/api/weatherAPI.ts
@@ -13,10 +13,11 @@ export const useFetchWeatherData = (
           `${process.env.REACT_APP_OW_API_URL}?q=${city}&lang=${process.env.REACT_APP_OW_API_LANG}&appid=${process.env.REACT_APP_OW_API_KEY}`
         )
         .then((response: AxiosResponse<WeatherData>) => {
+          console.log(`Fetched weather data for ${city}:`, response.data); // 都市名をログに追加
           setWeatherData(response.data);
         })
         .catch((error: AxiosError) => {
-          console.error('Error fetching the weather data', error);
+          console.error(`Error fetching the weather data for ${city}`, error); // エラーメッセージにも都市名を追加
         });
     }
   }, [city, setWeatherData]);
