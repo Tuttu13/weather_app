0901ba3979c0bd97951478cc3a8c9595ed7c2f03
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-06-06 00:57:09 +0900

    都市選択時の型設定　追加

--- react-ts-app/src/weather/utils/weatherFunctions.ts
+++ react-ts-app/src/weather/utils/weatherFunctions.ts
@@ -4,7 +4,7 @@ export const useHandleCityChange = (
   setCity: React.Dispatch<React.SetStateAction<string>>
 ) => {
   const handleCityChange = (event: SelectChangeEvent<string>) => {
-    setCity(event.target.value);
+    setCity(event.target.value as string);
   };
 
   return handleCityChange;
